a9cc993e2982f7de32d9b3f0af8f91d62e57a955
hyan <tristan_yan@intuit.com>
2016-05-09 22:23:57 -0700

    Add more test for wstring for removing algorithm and mutating algorithm

--- stl/src/mutatingAlgorithm.cpp
+++ stl/src/mutatingAlgorithm.cpp
@@ -1,8 +1,18 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <list>
 
 using namespace std;
+
+void testReverse() {
+    list<float> li{3.14159, 2.71828, 1.61803};
+    reverse(li.begin(), li.end());
+    copy(li.cbegin(), li.cend(), ostream_iterator<float>(cout, " "));
+    cout << endl;
+    copy(li.rbegin(), li.rend(), ostream_iterator<float>(cout, " "));
+    cout << endl;
+}
 
 void printPermutate(vector<int> &v) {
     copy(v.cbegin(), v.cend(), ostream_iterator<int>(cout, " "));
@@ -14,6 +24,8 @@ void printPermutate(vector<int> &v) {
 }
 
 auto main() -> int {
+    testReverse();
+
     vector<int> v1{1,2,3};
     vector<int> v2{1,1, 2,3};
     printPermutate(v1);
---
+++ stl/src/removingAlgorithm.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void testRemoveElem() {
+    vector<wstring> vs1 = {L"儿", L"嬉", L"漫", L"说", L"重", L"重", L"午"};
+    auto iter = remove(vs1.begin(), vs1.end(), L"漫");
+    vs1.erase(iter, vs1.end());
+    copy(vs1.cbegin(), vs1.cend(), ostream_iterator<wstring, wchar_t>(wcout));
+    wcout << endl;
+
+    vs1 = {L"儿", L"嬉", L"漫", L"说", L"重", L"重", L"午"};
+    vs1.erase(remove_if(vs1.begin(), vs1.end(), [](wstring elem){
+          return elem == L"重";
+    }));
+    copy(vs1.cbegin(), vs1.cend(), ostream_iterator<wstring, wchar_t>(wcout));
+    wcout << endl;
+
+    vs1 = {L"儿", L"嬉", L"漫", L"说", L"重", L"重", L"午"};
+    unique(vs1.begin(), vs1.end());
+    copy(vs1.cbegin(), vs1.cend(), ostream_iterator<wstring, wchar_t>(wcout));
+    wcout << endl;
+}
+
+auto main() -> int {
+  locale::global(locale(""));
+  wcout.imbue(locale(""));
+  testRemoveElem();
+}
