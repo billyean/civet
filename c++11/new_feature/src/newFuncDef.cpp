1484e927e3c17182da7df371becf3f4b3519d7d4
Tristan Yan <tristan.yim@gmail.com>
2016-05-14 00:12:55 -0700

    add new Template function definition

---
+++ c++11/new_feature/src/newFuncDef.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+class MyString : public string {
+  string instance;
+public:
+    MyString(string s) : instance(s) {}
+    MyString& operator+(int i)
+    {
+      MyString *vs = new Mystring(v + to_string(i));
+      return *vs;
+    }
+    friend ostream& operator<<(ostream& , const MyString&);
+};
+
+// Override << for MyString class
+ostream& operator<<(ostream& os, const MyString& ms) {
+    os << ms.instance;
+    return os;
+}
+
+// We couldn't do this decltype(x + y) before C++ 11.
+template<typename T1, typename T2>
+auto add(T1 x, T2 y) -> decltype(x + y) {
+    return x + y;
+}
+
+auto main() -> int {
+    Mystring hs{"hello"};
+    auto appendIntToStr = add(hs, 9);
+    cout << appendIntToStr << endl;
+
+    auto appendIntToDouble= add(2.3, 9);
+    cout << appendIntToDouble << endl;
+}
