0f0c895c8ee9eef2a62dc458cb5bc9201327ade5
Haibo Yan <haibo.yan@bytedance.com>
2023-08-01 23:09:33 -0700

    Update main

--- pushbase/src/main.cpp
+++ pushbase/src/main.cpp
@@ -6,8 +6,19 @@
 #include <future>
 #include <iostream>
 #include <cmath>
+#include <pthread.h>
 
 using namespace std;
+
+
+bool isPrime(long candidate) {
+  for (long j = 2; j <= sqrt(candidate); j++) {
+    if (candidate % j == 0) {
+      return false;
+    }
+  }
+  return true;
+}
 
 auto main() -> int {
   MyThreadPool pool;
@@ -18,74 +29,58 @@ auto main() -> int {
       case 0:
         results.emplace_back(pool.asyncall([](){
           long sum = 0;
-          for (int i = 1; i <= 1000000; i++) {
+          for (int i = 1; i <= INT16_MAX; i++) {
             sum += i;
           }
-          cout << "Sum of [1, 1000000] is " << sum << "." << endl;
+          cout << "In thread[" << pthread_self() << "]: calculate sum of [1, " << INT16_MAX <<  "] is " << sum << "." << endl;
         }));
         break;
       case 1:
         results.emplace_back(pool.asyncall([](){
           long sum = 0;
-          for (int i = 1; i <= 1000000; i++) {
+          for (long i = 1; i < INT32_MAX - 1; i++) {
             sum += i * (i + 1);
           }
-          cout << "Sum of [1, 1000000] product [2, 1000001] is " << sum << "." << endl;
+          cout << "In thread[" << pthread_self() << "]: calculate sum of [1, " << INT32_MAX - 1 << "] product [2, " << INT32_MAX << "] is " << sum << "." << endl;
         }));
         break;
       case 2:
         results.emplace_back(pool.asyncall([](){
           long prime = 8;
           int i = 4;
-          while (i < 1000) {
-            for (int j = 2; j <= sqrt(prime); j++) {
-              if (prime % j == 0) {
-                i++;
-                prime++;
-                break;
-              }
-            }
-            prime++;
+          while (i < 10000) {
+            while (!isPrime(++prime)){}
+            i++;
           }
-          cout << "1000th prime number is " << prime << "." << endl;
+          cout << "In thread[" << pthread_self() << "]: found 10000th prime number is " << prime << "." << endl;
         }));
         break;
       case 3:
         results.emplace_back(pool.asyncall([](){
-          long fibonacci0 = 0;
-          long fibonacci = 1;
+          long long fibonacci0 = 0;
+          long long fibonacci = 1;
           for (int i = 1; i < 10000; i++) {
-            long temp = fibonacci + fibonacci0;
+            long long temp = fibonacci + fibonacci0;
             fibonacci0 = fibonacci;
             fibonacci = temp;
           }
-          cout << "10000th fibonacci number is " << fibonacci << "." << endl;
+          cout << "In thread[" << pthread_self() << "]: found 10000th fibonacci number is " << fibonacci << "." << endl;
         }));
         break;
       default:
         results.emplace_back(pool.asyncall([](){
           srand((unsigned) time(NULL));
           std::vector<long> primes;
-          long upperbound = 9999999999;
-          long lowerbound = 1000000000;
-          for (int i = 0; i < 4; i++) {
-            bool not_prime = true;
-            while (not_prime) {
-              long randNum = rand() % (upperbound - lowerbound + 1) + lowerbound;
-              bool found = true;
-              for (int j = 2; j <= sqrt(randNum); j++) {
-                if (randNum % j == 0) {
-                  found = false;
-                  break;
-                }
-              }
-              if (found) {
-                not_prime = false;
-                primes.push_back(randNum);
-              }
+          long upperbound = 99999999999;
+          long lowerbound = 10000000000;
+          for (int i = 0; i < 20; i++) {
+            long randNum;
+            for(randNum = rand() % (upperbound - lowerbound + 1) + lowerbound; !isPrime(randNum);
+              randNum = rand() % (upperbound - lowerbound + 1) + lowerbound) {
             }
+            primes.push_back(randNum);
           }
-          cout << "Found 4 prime numbers between [1000000000, 9999999999] ";
+          cout << "In thread[" << pthread_self() << "]: Found 20 prime numbers between [1000000000, 9999999999] ";
           for (auto i = primes.begin(); i != primes.end(); ++i)
             cout << *i << ',';
           cout << endl;
@@ -104,3 +99,4 @@ auto main() -> int {
 
   return 0;
 }
+
